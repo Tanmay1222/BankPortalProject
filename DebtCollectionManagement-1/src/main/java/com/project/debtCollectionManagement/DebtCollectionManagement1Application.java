480b74fb2facfdacb9ed3675942c2cd791aaf902
Tanmay1222 <tanmayshelke060@gmail.com>
2024-06-22 14:09:32 +0530

    Removed eureka deps

--- DebtCollectionManagement-1/src/main/java/com/project/debtCollectionManagement/DebtCollectionManagement1Application.java
+++ DebtCollectionManagement-1/src/main/java/com/project/debtCollectionManagement/DebtCollectionManagement1Application.java
@@ -3,12 +3,13 @@ package com.project.debtCollectionManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.project.*")
 @EntityScan(basePackages = "com.project.entities")
 @EnableJpaRepositories(basePackages = "com.project.repositories")
+//@EnableDiscoveryClient(autoRegister = true)
 public class DebtCollectionManagement1Application {
 
 	public static void main(String[] args) {
